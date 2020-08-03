01b6b4ab6c1fd130045113c46cad86ee708e477d
lawiedc <github-dcl@hoopoes.com>
2020-08-03 14:36:27 +0100

    Add a default auth file format

---
+++ twitter_authentication_default.py
@@ -0,0 +1,6 @@
+# authentication information from Twitter to use the API
+# sourced via https://developer.twitter.com/en/apps
+consumer_key = "string"
+consumer_secret = "string"
+access_token = "string"
+access_secret = "string"
